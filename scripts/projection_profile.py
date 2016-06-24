40f302827b2fc851db45aff5790a553291346b8a
PragyaJaiswal <pragya.jswl@gmail.com>
2016-06-25 01:21:25 +0530

    Calculates Gray level histogram for image.

--- scripts/projection_profile.py
+++ scripts/projection_profile.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import math
 import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
@@ -9,11 +10,24 @@ import matplotlib.pyplot as plt
 data_dir = '../data/CXR_png/'
 
 def profile_one_dim(im):
+	gray_level(im)
 	vertical_sum = np.sum(im, axis=0)/np.shape(im)[1]
 	plt.plot(vertical_sum)
 	plt.show()
+
+def gray_level(im):
+	num_of_gray_levels = len(np.unique(im))
+	image_bit = math.log(num_of_gray_levels, 2)
+	gray_level_hist = np.zeros(2**image_bit)
+	print(np.shape(im))
+	for x in im:
+		for y in x:
+			gray_level_hist[y]+=1
+	print(gray_level_hist)
+
 
 if __name__ == '__main__':
 	for image in os.listdir(data_dir):
 		im = scipy.ndimage.imread(data_dir + image)
 		profile_one_dim(im)
+		input('Enter')
