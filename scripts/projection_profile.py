8f280612ab878b51b1bf3a1f3d0801fcaaf9cbc4
PragyaJaiswal <pragya.jswl@gmail.com>
2016-06-27 14:36:24 +0530

    Added maximum and minimum points; will be used for calculating features.

--- scripts/projection_profile.py
+++ scripts/projection_profile.py
@@ -19,7 +19,7 @@ def profile_one_dim(im):
     fig = plt.figure(0)
     fig.canvas.set_window_title('Projection Profile - ' + filename)
     plt.plot(vertical_sum)
-    plt.show()
+    # plt.show()
     zone_division(im, vertical_sum)
 
 def gray_level(im):
@@ -27,17 +27,21 @@ def gray_level(im):
     image_bit = math.log(num_of_gray_levels, 2)
     '''
     Initialise a gray_level_hist list with all zeros. Indices
-    denote the grap level, value at index denote the count.
+    denote the gray level, value at index denote the count.
     '''
-    gray_level_hist = np.zeros(2**image_bit)
-    # # VERY SLOW
+    # VERY SLOW
+    # gray_level_hist = np.zeros(2**image_bit)
     # for x in im:
     #   for y in x:
     #       gray_level_hist[y]+=1
     # print(gray_level_hist)
 
     unique, counts = np.unique(im, return_counts=True)
-    gray_level_hist_dict = dict(zip(unique, counts))
+    gray_level_hist_dict = dict(zip(unique, counts))    # keys denote gray_level, values denote gray level count
+    '''
+    background_value :- is the gray level at which the peak appears
+    close to the maximum value in the gray level histogram.
+    '''
     background_value = max(gray_level_hist_dict.items(), key=operator.itemgetter(1))[0]
     # print(background_value, gray_level_hist_dict[background_value])
     normalized_im = np.divide(im, background_value)
@@ -59,12 +63,12 @@ def zone_division(im, vertical_sum):
     '''
     x_right = math.floor(len(vertical_sum)/4)
     print(x_right, 'div')
-    vertical_profile(im, x_right)
+    vertical_profile_at_xright(im, x_right)
     '''
 
     x_right = list(vertical_sum).index(max(vertical_sum[low:high]))
     print(x_right, 'x_right')
-    vert_prof = vertical_profile(im, x_right)
+    vert_prof = vertical_profile_at_xright(im, x_right)
 
     # For ytop
     def ytop():
@@ -78,7 +82,7 @@ def zone_division(im, vertical_sum):
         fig = plt.figure(0)
         fig.canvas.set_window_title('Vertical Profile at x_right - ' + filename)
         plt.plot(vert_prof)
-        plt.show()
+        # plt.show()
         return ytopi
 
 
@@ -100,7 +104,7 @@ def zone_division(im, vertical_sum):
         fig = plt.figure(0)
         fig.canvas.set_window_title('Vertical Profile Derivative at x_right - ' + filename)
         plt.plot(vert_prof_derivative)
-        plt.show()
+        # plt.show()
         return ybottomi
 
     ytopi = ytop()
@@ -121,15 +125,63 @@ def zone_division(im, vertical_sum):
     Pz2 = np.sum(im[y1:y2], axis=0)/chunks(y1, y2)
     Pz3 = np.sum(im[y2:y3], axis=0)/chunks(y2, y3)
     Pz4 = np.sum(im[y3:ybottomi], axis=0)/chunks(y3, ybottomi)
+    P = [Pz1, Pz2, Pz3, Pz4]
+    # print(P)
 
     fig = plt.figure(0)
     fig.canvas.set_window_title('Zone wise projection profile - ' + filename)
     plt.plot(Pz1, 'r', Pz2, 'g', Pz3, 'b', Pz4, 'r--')
-    plt.show()
+    # plt.show()
     
+    points_vector(P, vertical_sum)
 
 
-def vertical_profile(im, x_right):
+def points_vector(P, vertical_sum):
+    X = np.zeros((4, 5))
+    '''
+    X(4, 5) - 4 rows, one each for each local zone projection profile
+    X = [[xrrib  xrlung  xcenter    xllung  xlrib],
+         .
+         .
+         [xrrib  xrlung  xcenter    xllung  xlrib]]
+    '''
+    for i in range(0, len(P)):
+        print(len(P[i]))
+        # x_center
+        low = math.floor(0.25*len(P[i]))
+        high = math.floor(0.75*len(P[i]))
+        xc = np.argmin(np.asarray(P[i][low:high])) + low
+        X[i][2] = xc
+
+        # xrlung
+        low = math.floor(0.125*len(P[i]))
+        high = xc
+        xrlung = np.argmax(np.asarray(P[i][low:high])) + low
+        X[i][1] = xrlung
+
+        # xllung
+        low = xc
+        high = math.floor(0.875*len(P[i]))
+        xllung = np.argmax(np.asarray(P[i][low:high])) + xc
+        X[i][3] = xllung
+
+        # xrrib
+        low = 0
+        high = xrlung
+        xrrib = np.argmin(np.asarray(P[i][low:high]))
+        X[i][0] = xrrib
+
+        # xlrib
+        low = xllung
+        high = len(P[i]) - 1
+        xlrib = np.argmin(np.asarray(P[i][low:high])) + xllung
+        X[i][4] = xlrib
+
+    print(X)
+
+
+
+def vertical_profile_at_xright(im, x_right):
     vert_prof = []
     for x in im:
         vert_prof.append(x[x_right])
